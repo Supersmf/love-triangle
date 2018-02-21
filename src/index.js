79edfb90801363e2754484b23fc849c976774e61
spec16 <spec16@InzhAuto.local>
2018-02-21 08:21:31 +0200

    love-triangle/ feature: add algoritm

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+module.exports = function getLoveTrianglesCount(array = []) {
+    let res = [];
+    array.forEach((item, i, arr) => {
+        if (item > i && arr[item - 1] !== item) {
+            if (arr[arr[item - 1] - 1] === (i + 1)) {
+                res.push(item * arr[item - 1] * arr[arr[item - 1] - 1]);
+            }
+        }
+    });
+    res = res.filter((item, pos) => res.indexOf(item) == pos);
+    return res.length;
 };
